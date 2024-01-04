10c198a0259dbaa81236e62fdef8e97ddec177ae
pagmat-dev1 <eren.bilen@pagmat.com>
2024-01-04 10:38:21 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Restoran_Yonetim_Sistemi_v2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Restoran_Yonetim_Sistemi_v2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
